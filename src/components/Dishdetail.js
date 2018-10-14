68721df45fdadd64860d0b6422910c521c9235d5
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-10-14 17:25:44 +0200

    Feat: create Dishdetail component

---
+++ src/components/Dishdetail.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Dishdetail extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Dishdetail;
