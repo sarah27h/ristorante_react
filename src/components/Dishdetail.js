8085f4037d333317a4d75738a1c71f54694116ac
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-10-14 18:40:12 +0200

    Refactor: render selected dish details in a seperate component

--- src/components/Dishdetail.js
+++ src/components/Dishdetail.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+
 
 class Dishdetail extends Component {
 
@@ -7,9 +9,37 @@ class Dishdetail extends Component {
 
     }
 
-    render() {
-        return (
+    /* render selected dish with its details */
+    renderSelectedDish(dish) {
+        if(dish != null) {
+            {/* return dish details of clicked one*/}
+            return(
+                <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
 
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+            )
+        } else {
+            {/* return empty div when user didn't click any dish initial case*/}
+            return(
+                <div></div>
+            )
+        }
+    }
+
+    render() {
+        
+        {/* this div to display selectedDish details if user click on a dish */}
+        return (
+            <div className="row">
+                <div  className="col-12 col-md-5 m-1">
+                {this.renderSelectedDish(this.props.selectedDish)}
+                </div>
+            </div>
         );
     }
 }
