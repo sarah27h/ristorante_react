ba0265d4f9ee510c0751f37a72a1cd7384877c64
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-10-17 17:59:23 +0200

    Feat: renderComments of the selectedDish

--- src/components/Dishdetail.js
+++ src/components/Dishdetail.js
@@ -31,13 +31,38 @@ class Dishdetail extends Component {
         }
     }
 
+    renderComments(dish) {
+            if(dish != null) {
+                console.log(dish.comments);
+                const dishComments = dish.comments.map((comment) => {
+                    <p>{comment.rating}</p>
+                    {console.log(comment.rating)}
+                });
+                return(
+                    <div>
+                        <h4>Comments</h4>
+                        {dishComments}
+                    </div>
+                )
+            
+            } else {
+                return(
+                    <div></div>
+                )
+            }
+    }
+
     render() {
         
         {/* this div to display selectedDish details if user click on a dish */}
         return (
             <div className="row">
                 <div  className="col-12 col-md-5 m-1">
-                {this.renderSelectedDish(this.props.selectedDish)}
+                    {this.renderSelectedDish(this.props.selectedDish)}
+                </div>
+
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.selectedDish)}
                 </div>
             </div>
         );
