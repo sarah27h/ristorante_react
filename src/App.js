54aee1d3abc936d2e94bf20bbe4d8a85eacc52dc
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-10-23 19:46:37 +0200

    Update: <App/> to work with new container component <Main/>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar,NavbarBrand } from 'reactstrap';
-import Menu from './components/Menu.js';
-import { DISHES } from './data/dishes';
+import Main from './components/Main.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    // add dishes as a state into parent App component
-    this.state = {
-      dishes : DISHES
-    };
-  }
+  
   render() {
     return (
       <div>
-        <Navbar color="danger" light expand="md">
-            <NavbarBrand href="/">Best Resturant</NavbarBrand>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
