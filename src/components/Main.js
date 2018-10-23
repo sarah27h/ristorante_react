ad049ade062e797b062134ee2c5bfae1a1b0a2f2
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-10-23 19:37:43 +0200

    Feat: create <Main /> to be a container component

---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Navbar,NavbarBrand } from 'reactstrap';
+import Menu from './Menu.js';
+import { DISHES } from './data/dishes';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    // add dishes as a state into parent App component
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="danger" light expand="md">
+            <NavbarBrand href="/">Best Resturant</NavbarBrand>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+
+export default Main;
